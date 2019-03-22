a0d083bfbf729ccc819ea99aa93b795470fd71f8
Charith Jayasanka <Charithjayasanka2@gmail.com>
2019-03-22 09:25:00 +0530

    Tute 7 A added

--- Tute 07/CodingExerciseOneClient/src/codingexerciseoneclient/CodingExerciseOneClient.java
+++ Tute 07/CodingExerciseOneClient/src/codingexerciseoneclient/CodingExerciseOneClient.java
@@ -4,9 +4,10 @@
  * and open the template in the editor.
  */
 package codingexerciseoneclient;
+
 /**
  *
- * @author Charith Jayasanka
+ * @author 2017135
  */
 public class CodingExerciseOneClient {
 
@@ -14,12 +15,34 @@ public class CodingExerciseOneClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CodingExerciseOneClient client=new CodingExerciseOneClient();
+        // TODO code application logic here
+        CodingExerciseOneClient client = new CodingExerciseOneClient();
         client.executeTest();
     }
-    private void executeTest(){
+
+    private void executeTest() {
         System.out.println("[CLIENT] - Test Starting...");
-       
+
+        if (testConnection("Charith")) {
+            System.out.println("[CLIENT] - Server has returned: " + sendMessage("Hello !"));
+        } else {
+            System.out.println("[CLIENT] - Server is not reachable");
+        }
         System.out.println("[CLIENT] - Test Completed.");
     }
+
+  
+
+    private static String sendMessage(java.lang.String message) {
+        server.TestWebService_Service service = new server.TestWebService_Service();
+        server.TestWebService port = service.getTestWebServicePort();
+        return port.sendMessage(message);
+    }
+
+    private static Boolean testConnection(java.lang.String arg0) {
+        server.TestWebService_Service service = new server.TestWebService_Service();
+        server.TestWebService port = service.getTestWebServicePort();
+        return port.testConnection(arg0);
+    }
+
 }
--- Tute 07/CodingExerciseOneServer/src/java/Server/TestWebService.java
+++ Tute 07/CodingExerciseOneServer/src/java/Server/TestWebService.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package codingexerciseoneclient;
+package Server;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -16,13 +18,16 @@ import javax.jws.WebParam;
 @WebService(serviceName = "TestWebService")
 public class TestWebService {
 
+    String serverName = "CJ Server";
+
     /**
      * Web service operation
      */
     @WebMethod(operationName = "testConnection")
-    public Boolean testConnection() 
-    {
-        System.out.println("[SERVER] - Testing Connection");
+    public Boolean testConnection(String name) {
+        Date timeStamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("[SERVER - " + serverName + "] - Testing Connection");
+        System.out.println(timeStamp + " Connection from client " + name + " succeded");
         return true;
     }
 
@@ -30,8 +35,7 @@ public class TestWebService {
      * Web service operation
      */
     @WebMethod(operationName = "sendMessage")
-    public String sendMessage(@WebParam(name = "message") String message) 
-    {
+    public String sendMessage(@WebParam(name = "message") String message) {
         System.out.println("[SERVER] - Received: " + message);
         return "Server has received " + message;
     }
